e662b31ffb8c8c86ce5b91098b24fa5ed28a4f84
Ethan Peterson <ethan@petetech.net>
2019-01-11 19:54:21 -0500

    calculated average difference still need to convert to % #1

--- analyze.py
+++ analyze.py
@@ -6,18 +6,8 @@
 import subprocess
 
 infile = open("data.txt", "r")
-testString = infile.readline()
 
 data = []
-
-for i, line in enumerate(infile):
-    if i > 0: # skip first line in reading process because data here is not useful
-        #print(line)
-        break
-        pass
-    pass
-
-#parseLine(testString)
 
 def parseLine(string):
     #print(string)
@@ -25,12 +15,28 @@ def parseLine(string):
     parsed[2] = parsed[2].strip('\n')
     for i in range(0, len(parsed)):
         parsed[i] = int(parsed[i])
-    print(parsed)
+    #print(parsed)
     data.append({
         "steering" : parsed[0],
         "actuator" : parsed[1],
         "intended" : parsed[2]
     })
-parseLine(testString)
-print(data)
+
+
+# Collect data into dictionary
+for i, line in enumerate(infile):
+    if i > 0: # skip first line in reading process because data here is not useful
+        parseLine(line)
+
+# Process Data
+
+
+total = 0
+for i in data:
+    total = total + abs(i['actuator'] - i['intended'])
+    pass
+avg = total / len(data)
+# Display Results
+print("Average Difference Between Actuator Pos and Intended Pos is: " + str(avg))
+
 exit()
