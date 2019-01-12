702f190dda76273cd79bdd036dbfba992f648a63
Ethan Peterson <ethan@petetech.net>
2019-01-11 19:41:01 -0500

    set up line parsing function

--- analyze.py
+++ analyze.py
@@ -1,13 +1,30 @@
+# analyze.py
+# Ethan Peterson
+# Jan 11, 2019
+# Take Comma seperated actuator data and run analysis on it.
+
+import subprocess
+
 infile = open("data.txt", "r")
-
 testString = infile.readline()
-#print(len(testString))
 
-for i in infile:
-    #print(i)
+data = []
+
+for i, line in enumerate(infile):
+    if i > 0: # skip first line in reading process because data here is not useful
+        #print(line)
+        break
+        pass
     pass
 
-def parseLine(str):
-    pass
+#parseLine(testString)
 
+def parseLine(string):
+    #print(string)
+    parsed = string.split(',')
+    parsed[2] = parsed[2].strip('\n')
+    for i in range(0, len(parsed)):
+        parsed[i] = int(parsed[i])
+    print(parsed)
+parseLine(testString)
 exit()
